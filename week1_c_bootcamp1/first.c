7357a28917100f17a0c70afc80f17bb8b20cc371
IredaleJosh <146458819+IredaleJosh@users.noreply.github.com>
2023-10-09 17:41:41 +0000

    committed

--- week1_c_bootcamp1/first.c
+++ week1_c_bootcamp1/first.c
@@ -6,7 +6,7 @@ int main() {
     printf("Enter number of days: ");
     scanf("%d", &a);
     new_a = a * 24 * 3600;
-    dis = 300000000 * new_a;
+    dis = 3 * 10^8 * new_a;
     printf("The distance is %d\n", dis);
     return 0;
 }
