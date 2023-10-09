bff1d7872e4770422d455a5159dd863f032251c4
IredaleJosh <146458819+IredaleJosh@users.noreply.github.com>
2023-10-09 14:59:36 +0000

    committed

--- week1_c_bootcamp1/first.c
+++ week1_c_bootcamp1/first.c
@@ -1,10 +1,5 @@
-#include <stdio.h> //library
+#include <stdio.h>
 int main() {
-    int age;
-
-    printf("Hello, please enter your age: ");
-    scanf("%d", &age); // scanf takes user inputs, & -> address of operator, check slides
-    printf("Your age is %d\n", age); // \n is the "newline" character
-    
+    printf("Hello world");
     return 0;
 }
