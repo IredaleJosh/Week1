bc26c17e4c1ccaa2abd4a51e62fabe6d9d740eaa
IredaleJosh <146458819+IredaleJosh@users.noreply.github.com>
2023-11-20 10:58:39 +0000

    committed

--- coursework/StepCounter.c
+++ coursework/StepCounter.c
@@ -1,13 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "FitnessDataStruct.h"
 
-int main()
+// Struct moved to header file
+
+// Define any additional variables here
+char 
+// Global variables for filename and FITNESS_DATA array
+
+
+// This is your helper function. Do not change it in any way.
+// Inputs: character array representing a row; the delimiter character
+// Ouputs: date character array; time character array; steps character array
+void tokeniseRecord(const char *input, const char *delimiter,
+                    char *date, char *time, char *steps) 
 {
-    //Options
-    printf("A: Specify the filename to be imported\n");  
-    printf("B: Display the total number of records in the file\n");      
-    printf("C: Find the date and time of the timeslot with fewest steps\n");
-    printf("D: Find the date and time of the timeslot with the largest number of steps\n");     
-    printf("E: Find the mean step count of all records in the file\n");
-    printf("F: Find the largest continuous period where the step count is above 500 steps\n");
-    printf("Q: Exit the program\n");    
+    // Create a copy of the input string as strtok modifies the string
+    char *inputCopy = strdup(input);
+    
+    // Tokenize the copied string
+    char *token = strtok(inputCopy, delimiter);
+    if (token != NULL) {        strcpy(date, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(time, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(steps, token);
+    }
+    
+    // Free the duplicated string
+    free(inputCopy);
+}
+
+//functions for option A - F here
+FILE *input_filename(char *filename)
+{
+    FILE *filename = fopen(filename, "r");
+    if(!filename)
+    {
+        printf("Error: Could not fine or open the file");
+        return 1;
+    }
+    return filename
+}
+
+// Complete the main function
+int main() 
+{
+    //Loop through forever until program exits
+    while(1)
+    {
+        //Display choices
+        printf("Select one of the following options\n");
+        printf("A: Specify the filename to be imported\n");
+        printf("B: Display the total number of records in the file\n");
+        printf("C: Find the date and time of the timeslot with the fewest steps\n");
+        printf("D: Find the date and time of the timeslot with the largest steps\n");
+        printf("E: Find the mean step count of all the records in the file\n");
+        printf("F: Find the longes continous period where the step count is above 500 steps\n");
+        printf("Q: Exit program\n");
+
+        option = getchar();
+        while(getchar() != '\n');
+
+        //Options - make a function
+        switch(option)
+        {
+            case "A":
+                printf("Input Filename: ");
+                fgets(line, buffer_size, stdin);
+                sscanf(line, " %s ", filename);
+                FILE *input = input_filename();
+                break;
+            case "B":
+                //record_number
+                break;
+            case "C":
+                //fewest_steps
+                break;
+            case "D":
+                //largest_steps
+                break;
+            case "E":
+                //mean_steps
+                break;
+            case "F":
+                //longest_period
+                break;
+            case "Q":
+                return 0;
+                break;
+            default;
+                printf("Invalid choice, try again");
+                break;
+        }
+    }
 }
--- week7_bootcamp/first.c
+++ week7_bootcamp/first.c
@@ -1,2 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+int main()
+{
+    int array_1[3][4];
+    int array_2[3][4];
+    int array_3[3][4];
+    int i, j;
+
+    //seed the random num gen
+    srand(time(NULL));
+
+    //fill array
+    for(i = 0; i < 3; i++)
+    {
+        for(j = 0; j < 4; j++)
+        {
+            array_1[i][j] = rand() % 11;
+            array_2[i][j] = rand() % 11;
+        }
+    }
+
+    //print array
+    for(i = 0; i < 3; i++)
+    {
+        for(j = 0; j < 4; j++)
+        {
+            array_3[i][j] = array_1[i][j] * array_2[i][j];
+            printf("%d ", array_3[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
