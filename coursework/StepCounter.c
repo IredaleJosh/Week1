d88ccb29e21abd919d3101cbcff2f056435849d4
IredaleJosh <146458819+IredaleJosh@users.noreply.github.com>
2023-11-24 19:05:52 +0000

    committed

--- coursework/StepCounter.c
+++ coursework/StepCounter.c
@@ -6,10 +6,15 @@
 // Struct moved to header file
 
 // Define any additional variables here
+int record_num, step_counter, counter, need_counter, start, end, average;
+float temp_mean, mean;
+
 // Global variables for filename and FITNESS_DATA array
 #define buffer_size 100
 char line [buffer_size];
 char filename[buffer_size];
+char line_count[24];
+FITNESS_DATA step_reading [100];
 
 // This is your helper function. Do not change it in any way.
 // Inputs: character array representing a row; the delimiter character
@@ -39,6 +44,22 @@ void tokeniseRecord(const char *input, const char *delimiter,
     free(inputCopy);
 }
 
+FILE *openfile(char *filename)
+{
+    FILE *file = fopen(filename, "r");
+    if(file == NULL)
+    {
+        printf("Incorrect filename, try again");
+        exit(1);
+    }
+    else
+    {
+        printf("File loaded successfully\n");
+        return file;
+    }
+    
+}
+
 // Complete the main function
 int main() 
 {
@@ -56,40 +77,128 @@ int main()
         printf("F: Find the longes continous period where the step count is above 500 steps\n");
         printf("Q: Exit program\n");
 
+        //Must use these, otherwise thinks two characters are inputted
         option = getchar();
-
         while (getchar() != '\n');
 
         //Options - make a function
-        switch (option)
+        switch ( option)
         {
-            case "A":
+            case 'A':
+                //Get filename
                 printf("Input Filename: ");
                 fgets(line, buffer_size, stdin);
                 sscanf(line, " %s ", filename);
-                FILE *file = fopen(filename, "r");
-                if(file == NULL)
+                //Open file if possible
+                FILE *file = openfile(filename);
+
+                break;
+            case 'B':
+                record_num = 0;
+                while(fgets(line_count, 24, file) != NULL)
                 {
-                    printf("Incorrect filename, try again");
-                    return 1;
+                    record_num = record_num + 1;
                 }
+                printf("Total records: %d\n", record_num);
+                file = openfile(filename);
                 break;
-            case "B":
-                //record_number
+            case 'C':
+                counter = 0;
+                need_counter = 0; //stores the line of where the fewest steps are
+                while(fgets(line, buffer_size, file))
+                {
+                    char char_steps [5];
+                    int int_steps;
+                    tokeniseRecord(line, ",", step_reading[counter].date, step_reading[counter].time, char_steps);
+                    int_steps = atoi(char_steps);
+                    if (counter == 0)
+                    {
+                        step_counter = int_steps;
+                    }
+                    else if (int_steps < step_counter)
+                    {
+                        need_counter = counter;
+                        step_counter = int_steps;
+                    }
+                    counter++;
+                }
+                printf("Fewest steps: %s %s\n", step_reading[need_counter].date, step_reading[need_counter].time);
+                file = openfile(filename);
                 break;
-            case "C":
-                //fewest_steps
+            case 'D':
+                counter = 0;
+                need_counter = 0; //stores the line of where the fewest steps are
+                while(fgets(line, buffer_size, file))
+                {
+                    char char_steps [5];
+                    int int_steps;
+                    tokeniseRecord(line, ",", step_reading[counter].date, step_reading[counter].time, char_steps);
+                    int_steps = atoi(char_steps);
+                    if (int_steps > step_counter)
+                    {
+                        need_counter = counter;
+                        step_counter = int_steps;
+                    }
+                    counter++;
+                }
+                printf("Largest steps: %s %s\n", step_reading[need_counter].date, step_reading[need_counter].time);
+                file = openfile(filename);
                 break;
-            case "D":
-                //largest_steps
+            case 'E':
+                counter = 0;
+                while(fgets(line, buffer_size, file))
+                {
+                    char char_steps [5];
+                    int int_steps;
+                    tokeniseRecord(line, ",", step_reading[counter].date, step_reading[counter].time, char_steps);
+                    int_steps = atoi(char_steps);
+                    mean = int_steps + mean;
+                    counter++;
+                }
+                average = mean / counter;
+                temp_mean = (mean/(float)counter) - average;
+                if(temp_mean >= 0.5)
+                {
+                    average++;
+                }
+                printf("Mean step count: %d\n", average);
+                file = openfile(filename);
                 break;
-            case "E":
-                //mean_steps
+            case 'F':
+                counter = 0;
+                int con = 1;
+                while(fgets(line, buffer_size, file))
+                {
+                    char char_steps [5];
+                    int int_steps;
+                    tokeniseRecord(line, ",", step_reading[counter].date, step_reading[counter].time, char_steps);
+                    int_steps = atoi(char_steps);
+
+                    if (int_steps >= 500 && con == 1)
+                    {
+                        need_counter++;
+                    }
+                    else if (int_steps < 500 && con == 1)
+                    {
+                        if (end - start < need_counter)
+                        {
+                            end = counter;
+                            start = counter - need_counter;
+                        }
+                        need_counter = 0;
+                        con = 0;
+                    }
+                    else if (int_steps > 500 && con == 0)
+                    {
+                        con = 1;
+                    }
+                    counter++;
+                }
+                printf("Longest period start: %s %s\n", step_reading[start].date, step_reading[start].time);
+                printf("Longest period end: %s %s\n", step_reading[end].date, step_reading[end].time);
+                file = openfile(filename);
                 break;
-            case "F":
-                //longest_period
-                break;
-            case "Q":
+            case 'Q':
                 return 0;
                 break;
             default:
