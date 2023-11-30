7b4f5a333dcb25717860338f11e73e8df49a97c1
IredaleJosh <146458819+IredaleJosh@users.noreply.github.com>
2023-11-30 10:10:29 +0000

    commited

--- coursework_3/FitnessDataSorter.c
+++ coursework_3/FitnessDataSorter.c
@@ -26,8 +26,52 @@ void tokeniseRecord(char *record, char delimiter, char *date, char *time, int *s
     }
 }
 
+//Sort data from highest step at top to lowest
+//in tab seperated values (.tsv) format - make space via \t
+
+//1.2) check file is valid, AND data is correct format, then print out msg and return 1
+//2) read file into an array, and sort data into descending order (if same step count, be
+//consecutive but order doesn't matter)
+//3) write data to filename of same name, with file extenstion .tsv and return 0
 
 int main() 
 {
-    
+    //Variables
+    #define buffer_size 100
+    char line [buffer_size];
+    char filename [buffer_size];
+    FitnessData read [100];
+    char char_steps [5];
+    int counter, int_steps;
+    char del [] = ",";
+
+    //Get filename
+    printf("Enter filename: ");
+    fgets(line, buffer_size, stdin);
+    sscanf(line, " %s ", filename);
+    //Open file
+    FILE *file = fopen(filename, "r");
+    //Check if valid
+    if(file == NULL)
+    {
+        printf("Error: Incorrect File\n");
+        return 1;
+    }
+    //Check if correct format
+    while(fgets(line, buffer_size, file))
+    {
+        tokeniseRecord(line, *del , read[counter].date, read[counter].time, &read[counter].steps);
+        //Check if date and time is empty
+        if (read[counter].date == " " || read[counter].time == " ")
+        {
+            printf("Error: Incorrect File\n");
+            return 1;
+        }
+        counter++;
+    }
+
+    //Read file into array
+
+    //Sort into new file with .tsv
+
 }
