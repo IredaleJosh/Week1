766dc050acddd3cbe079b3bb6ac7ab67db6acf4e
IredaleJosh <146458819+IredaleJosh@users.noreply.github.com>
2023-12-04 10:54:20 +0000

    commited

--- coursework_3/FitnessDataSorter.c
+++ coursework_3/FitnessDataSorter.c
@@ -30,9 +30,26 @@ void tokeniseRecord(char *record, char delimiter, char *date, char *time, int *s
 //in tab seperated values (.tsv) format - make space via \t
 
 //1.2) check file is valid, AND data is correct format, then print out msg and return 1
-//2) read file into an array, and sort data into descending order (if same step count, be
+//2) and sort data into descending order (if same step count, be
 //consecutive but order doesn't matter)
 //3) write data to filename of same name, with file extenstion .tsv and return 0
+
+
+
+// int comparator(const void *a, const void *b)
+// {
+//     FitnessData *FitnessData_A = (FitnessData *)a;
+//     FitnessData *FitnessData_B = (FitnessData *)b;
+//     return (FitnessData_A-> steps - FitnessData_B-> steps);
+// }
+
+    void swap(int* a, int* b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
 int main() 
 {
@@ -40,9 +57,10 @@ int main()
     #define buffer_size 100
     char line [buffer_size];
     char filename [buffer_size];
-    char char_steps [5];
-    int counter = 0, int_steps;
+    int counter = 0, counter_2 = 0, size = 0;
     FitnessData Store_Array [buffer_size];
+    FitnessData Sort_Array [buffer_size];
+    FitnessData read;
 
     //Get filename
     printf("Enter filename: ");
@@ -56,25 +74,48 @@ int main()
         printf("Error: Incorrect File\n");
         return 1;
     }
-    //Check store into an array
+
+    //Check if correct format
+
+    //Store into an array
     while(fgets(line, buffer_size, file))
     {
-        FitnessData read;
         tokeniseRecord(line, ',', read.date, read.time, &read.steps);
         Store_Array[counter] = read;
         counter++;
     }
-    for(int i = 0; i < counter; i++)
+
+        //Sort array
+    int swapped = 1;
+    for (int i = 0; i < counter - 1; i++)
     {
-        if(Store_Array[i].time == NULL)
+        swapped = 0; //Starts False
+        for (int j = 0; j < (counter - i - 1); j++)
         {
-            printf("Error");
-            return 1;
+            //if current element bigger than next one
+            if(Store_Array[j].steps > Store_Array[j + 1].steps)
+            {
+                swap(&Store_Array[j].steps, &Store_Array[j+1].steps); //Swap steps
+                //Swap Time
+                //Swap Date
+                swapped = 1; //Set to True
+            }
+        }
+        if(swapped == 0) //Never becomes true / no swap
+        {
+            break;
         }
     }
 
-    //Read file into array
+    for(int i; i < counter; i++)
+    {
+        printf("%s %s %d\n", Store_Array[i].date, Store_Array[i].time, Store_Array[i].steps);
+    }
+
+
 
     //Sort into new file with .tsv
+
+    return 0;
 
 }
